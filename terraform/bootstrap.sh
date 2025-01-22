2eaefb577261124a381fe8d931c84db055f289ab
thulasiguddeti <thulasiguddeti02@gmail.com>
2025-01-22 12:19:45 +0530

    new

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 component=$1
 environment=$2 #dont use env here, it is reserved in linux
-app_version =$3
+app_version=$3
 dnf install ansible -y 
 ansible-pull -U https://github.com/thulasiguddeti/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
